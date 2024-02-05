84366c7e403883f49144173222ad78614f00a1ad
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-02-05 10:28:16 -0500

    l5/2 - read blog post list

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,20 @@
 let express = require('express');
 let router = express.Router();
 
+// Post model for CRUD
+let Post = require('../models/post');
+
 /* GET: /posts => show main blog page */
-router.get('/', (req, res) => {
-    res.render('posts/index');
+router.get('/', async(req, res) => {
+    // use model to fetch all Post data from MongoDB
+    let posts = await Post.find();
+    // console.log(posts[0]);
+
+    // load view and pass all the json data to it for display
+    res.render('posts/index', {
+        title: 'Our Lastest Random Thoughts',
+        posts: posts
+    });
 });
 
 // make public
